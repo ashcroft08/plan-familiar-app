ef2a749a4a9c55b06dbe5177df3a5bbae09ec644
Joseph <108558862+ashcroft08@users.noreply.github.com>
2024-10-23 09:47:06 -0500

    Update de archivos html

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,30 @@
 |
 */
 
-$router->get('/', function () use ($router) {
+/*$router->get('/', function () use ($router) {
     return $router->app->version();
+});*/
+
+$router->get('/', function () use ($router) {
+    return file_get_contents(resource_path('views/dashboard.html'));
+});
+
+$router->get('/informacion_general', function () {
+    return view('informacion_general'); 
+});
+
+$router->get('/lugares_de_evacuacion_y_de_encuentro', function () {
+    return view('lugares_de_evacuacion_y_de_encuentro'); 
+});
+
+$router->get('/integrantes_de_la_familia', function () {
+    return view('integrantes_de_la_familia'); 
+});
+
+$router->get('/identificacion_de_amenazas', function () {
+    return view('identificacion_de_amenazas'); 
+});
+
+$router->get('/recursos_familiares_disponibles', function () {
+    return view('recursos_familiares_disponibles'); 
 });
